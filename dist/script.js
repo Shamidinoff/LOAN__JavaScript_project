4411b7b02b72fc3cd1cac54dcf7d84ebfc7be338
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-10-09 21:06:32 +0600

    LOAN - JavaScript project

--- dist/script.js
+++ dist/script.js
@@ -26,6 +26,18 @@ class Slider {
     if (n < 1) {
       this.slideIndex = this.slides.length;
     }
+    try {
+      this.hanson.style.opacity = "0";
+      if (n === 3) {
+        this.hanson.classList.add("animated");
+        setTimeout(() => {
+          this.hanson.style.opacity = "1";
+          this.hanson.classList.add("slideInUp");
+        }, 3000);
+      } else {
+        this.hanson.classList.remove("slideInUp");
+      }
+    } catch (e) {}
 
     // this.slides.forEach((slide) => {
     //   slide.style.display = "none";
@@ -39,6 +51,9 @@ class Slider {
     this.showSlides(this.slideIndex += n);
   }
   render() {
+    try {
+      this.hanson = document.querySelector(".hanson");
+    } catch (e) {}
     this.btns.forEach(item => {
       item.addEventListener("click", () => {
         this.plusSlides(1);
