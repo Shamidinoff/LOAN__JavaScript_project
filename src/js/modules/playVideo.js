3c3d22de6f9829a64911434708186f2e4bb6219e
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-10-09 21:39:47 +0600

    LOAN - JavaScript project

--- src/js/modules/playVideo.js
+++ src/js/modules/playVideo.js
@@ -5,7 +5,23 @@ export default class VideoPlayer {
     this.close = this.overlay.querySelector(".close");
   }
 
-  play() {
+  //   play() {}
+
+  createPlayer(url) {
+    this.player = new YT.Player("player", {
+      height: "360",
+      width: "640",
+      videoId: "M7lc1UVf-VE",
+    });
+  }
+
+  init() {
+    const tag = document.createElement("script");
+
+    tag.src = "https://www.youtube.com/iframe_api";
+    const firstScriptTag = document.getElementsByTagName("script")[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
     this.btns.forEach((btn) => {
       btn.addEventListener("click", () => {
         console.log("Hello");
