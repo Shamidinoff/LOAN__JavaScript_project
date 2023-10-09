cace5f8271104debd96054f770f678df5b0d4cfc
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-10-09 20:25:48 +0600

    LOAN - JavaScript project

--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -3,9 +3,35 @@ export default class Slider {
     this.page = document.querySelector(page);
     this.slides = this.page.children;
     this.btns = document.querySelectorAll(btns);
+    this.slideIndex = 1;
+  }
+
+  showSlides(n) {
+    if (n > this.slides.length) {
+      this.slideIndex = 1;
+    }
+    if (n < 1) {
+      this.slideIndex = this.slides.length;
+    }
+
+    this.slides.forEach((slide) => {
+      slide.style.display = "none";
+    });
+
+    this.slides[this.slideIndex - 1].style.display = "block";
+  }
+
+  plusSlides(n) {
+    this.showSlides((this.slideIndex += n));
   }
 
   render() {
-    console.log(this.page, this.slides);
+    this.btns.forEach((item) => {
+      item.addEventListener("click", () => {
+        this.plusSlides(1);
+      });
+    });
+
+    this.showSlides(this.slideIndex);
   }
 }
