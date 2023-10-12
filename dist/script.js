a9d4fe45bdf3c4352e3f30a6e6c5a3c6136023d3
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-10-12 21:23:50 +0600

    LOAN - JavaScript project

--- dist/script.js
+++ dist/script.js
@@ -85,6 +85,45 @@ class Form {
       });
     });
   }
+  initMask() {
+    let setCursorPosition = (pos, elem) => {
+      elem.focus();
+      if (elem.setSelectionRange) {
+        elem.setSelectionRange(pos, pos);
+      } else if (elem.createTextRange) {
+        let range = elem.createTextRange();
+        range.collapse(true);
+        range.moveEnd("character", pos);
+        range.moveStart("character", pos);
+        range.select();
+      }
+    };
+    function createMask(event) {
+      let matrix = "+1 (___) ___-____",
+        i = 0,
+        def = matrix.replace(/\D/g, ""),
+        val = this.value.replace(/\D/g, "");
+      if (def.length >= val.length) {
+        val = def;
+      }
+      this.value = matrix.replace(/./g, function (a) {
+        return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? "" : a;
+      });
+      if (event.type === "blur") {
+        if (this.value.length == 2) {
+          this.value = "";
+        }
+      } else {
+        setCursorPosition(this.value.length, this);
+      }
+    }
+    let inputs = document.querySelectorAll('[name="phone"]');
+    inputs.forEach(input => {
+      input.addEventListener("input", createMask);
+      input.addEventListener("focus", createMask);
+      input.addEventListener("blur", createMask);
+    });
+  }
   async postData(url, data) {
     let res = await fetch(url, {
       method: "POST",
@@ -93,6 +132,7 @@ class Form {
     return await res.text();
   }
   init() {
+    this.checkMailInputs();
     this.forms.forEach(item => {
       item.addEventListener("submit", e => {
         e.preventDefault();
--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -28,6 +28,58 @@ export default class Form {
     });
   }
 
+  initMask() {
+    let setCursorPosition = (pos, elem) => {
+      elem.focus();
+
+      if (elem.setSelectionRange) {
+        elem.setSelectionRange(pos, pos);
+      } else if (elem.createTextRange) {
+        let range = elem.createTextRange();
+
+        range.collapse(true);
+        range.moveEnd("character", pos);
+        range.moveStart("character", pos);
+        range.select();
+      }
+    };
+
+    function createMask(event) {
+      let matrix = "+1 (___) ___-____",
+        i = 0,
+        def = matrix.replace(/\D/g, ""),
+        val = this.value.replace(/\D/g, "");
+
+      if (def.length >= val.length) {
+        val = def;
+      }
+
+      this.value = matrix.replace(/./g, function (a) {
+        return /[_\d]/.test(a) && i < val.length
+          ? val.charAt(i++)
+          : i >= val.length
+          ? ""
+          : a;
+      });
+
+      if (event.type === "blur") {
+        if (this.value.length == 2) {
+          this.value = "";
+        }
+      } else {
+        setCursorPosition(this.value.length, this);
+      }
+    }
+
+    let inputs = document.querySelectorAll('[name="phone"]');
+
+    inputs.forEach((input) => {
+      input.addEventListener("input", createMask);
+      input.addEventListener("focus", createMask);
+      input.addEventListener("blur", createMask);
+    });
+  }
+
   async postData(url, data) {
     let res = await fetch(url, {
       method: "POST",
@@ -37,6 +89,7 @@ export default class Form {
   }
 
   init() {
+    this.checkMailInputs();
     this.forms.forEach((item) => {
       item.addEventListener("submit", (e) => {
         e.preventDefault();
